4db2278980ec245c56cc3263eb9bcb582275897c
amir <amir.h.rassafi@gmail.com>
2024-02-04 22:18:18 +0000

    Add log

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import asyncio
 from configparser import ConfigParser
 from src.influx_client import MonitoringClient 
 from src.v2ray_health import generate_config_create_clients
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 HOSTS = [
         'https://www.instagram.com',
@@ -16,6 +19,7 @@ influx_config = config["influxdb"]
 
 async def main():
     clients = generate_config_create_clients()
+    LOGGER.info('Start with {} nodes'.format(len(clients)))
     monitoring_client = MonitoringClient(
             url=influx_config['url'],
             token=influx_config['token'],
@@ -24,12 +28,14 @@ async def main():
             prob_name=config['app']['prob_name']
             )
     while True:
-        for client in clients:
-            for url in HOSTS:
-                response_time = await client.get(url)
-                await monitoring_client.send_point(client.tag, url, client.protocol, response_time)
-        await asyncio.sleep(int(config['app'].get('sampling_time', '10')))
-
+        try:
+            for client in clients:
+                for url in HOSTS:
+                    response_time = await client.get(url)
+                    await monitoring_client.send_point(client.tag, url, client.protocol, response_time)
+            await asyncio.sleep(int(config['app'].get('sampling_time', '10')))
+        except Exception as e:
+            LOGGER.error(e)
 
 
 if __name__ == '__main__':
